83654edd93994b931e819db35991289bb27ab4d3
EINazare <emanuelioannazare@gmail.com>
2020-04-21 11:24:21 +0300

    mutants

---
+++ src/MuJava_HOME/result/BBSecurity/traditional_mutants/void_generateResults()/AOIS_209/BBSecurity.java
@@ -0,0 +1,188 @@
+// This is a mutant program.
+// Author : ysma
+
+import java.lang.Math.*;
+import java.util.*;
+
+
+class BBSecurity
+{
+
+    private int n;
+
+    private double a;
+
+    private double b;
+
+    java.util.List<Double> l;
+
+    java.util.List<Double> c;
+
+    java.util.List<Double> h;
+
+    java.util.List<Double> result;
+
+    java.util.List<String> resultInInterval;
+
+    private java.lang.String resultOutput;
+
+    public BBSecurity( java.lang.String[] args )
+    {
+        resultOutput = "";
+        if (args.length < 3) {
+            resultOutput = "Invalid input";
+        } else {
+            boolean validA = true;
+            boolean validB = true;
+            boolean validN = true;
+            try {
+                n = Integer.parseInt( args[0] );
+            } catch ( java.lang.Exception e ) {
+                validN = false;
+            }
+            try {
+                a = Integer.parseInt( args[1] );
+                a = Double.parseDouble( args[1] );
+            } catch ( java.lang.Exception e ) {
+                validA = false;
+            }
+            try {
+                b = Integer.parseInt( args[2] );
+                b = Double.parseDouble( args[2] );
+            } catch ( java.lang.Exception e ) {
+                validB = false;
+            }
+            if (validN && (n < 1 || n > 100000)) {
+                validN = false;
+            }
+            if (validA && (a < 0 || a > 1000000)) {
+                validA = false;
+            }
+            if (validB && (b < 0 || b > 1000000)) {
+                validB = false;
+            }
+            if (args.length < 3 * n + 3 || !validA || !validB || !validN) {
+                resultOutput = "Invalid input";
+            } else {
+                l = new java.util.ArrayList<Double>();
+                c = new java.util.ArrayList<Double>();
+                h = new java.util.ArrayList<Double>();
+                result = new java.util.ArrayList<Double>();
+                resultInInterval = new java.util.ArrayList<String>();
+                double res;
+                double lArgs = -1;
+                double cArgs = -1;
+                double hArgs = -1;
+                boolean validL = true;
+                boolean validC = true;
+                boolean validH = true;
+                for (int i = 0; i < 3 * n; i = i + 3) {
+                    validL = true;
+                    validC = true;
+                    validH = true;
+                    try {
+                        lArgs = Integer.parseInt( args[i + 3] );
+                        lArgs = Double.parseDouble( args[i + 3] );
+                    } catch ( java.lang.Exception e ) {
+                        validL = false;
+                    }
+                    try {
+                        cArgs = Integer.parseInt( args[i + 4] );
+                        cArgs = Double.parseDouble( args[i + 4] );
+                    } catch ( java.lang.Exception e ) {
+                        validC = false;
+                    }
+                    try {
+                        hArgs = Integer.parseInt( args[i + 5] );
+                        hArgs = Double.parseDouble( args[i + 5] );
+                    } catch ( java.lang.Exception e ) {
+                        validH = false;
+                    }
+                    if (validL && (lArgs < 0 || lArgs > 1000000)) {
+                        validL = false;
+                    }
+                    if (validC && (cArgs < 0 || cArgs > 1000000)) {
+                        validC = false;
+                    }
+                    if (validH && (hArgs < 0 || hArgs > 1000000)) {
+                        validH = false;
+                    }
+                    if (!validL || !validC || !validH) {
+                        break;
+                    }
+                    res = inputResult( Double.parseDouble( args[i + 3] ), Double.parseDouble( args[i + 4] ), Double.parseDouble( args[i + 5] ) );
+                    l.add( Double.parseDouble( args[i + 3] ) );
+                    c.add( Double.parseDouble( args[i + 4] ) );
+                    h.add( Double.parseDouble( args[i + 5] ) );
+                    result.add( res );
+                    if (a <= res && res <= b) {
+                        resultInInterval.add( "da" );
+                    } else {
+                        resultInInterval.add( "nu" );
+                    }
+                }
+                if (validL && validC && validH) {
+                    generateResults();
+                } else {
+                    resultOutput = "Invalid input";
+                }
+            }
+        }
+    }
+
+    private  double asinh( double x )
+    {
+        return Math.log( x + Math.sqrt( x * x + 1.0 ) );
+    }
+
+    private  double inputResult( double l, double c, double h )
+    {
+        if (h == l) {
+            return c;
+        }
+        double a = -(l - h + c / 2) * (l - h - c / 2) / (2 * (l - h));
+        double res = 2 * a * asinh( 50 / a );
+        java.lang.String resString = Double.toString( res );
+        java.lang.String[] parts = resString.split( "\\." );
+        java.lang.String part1 = parts[0];
+        java.lang.String part2 = parts[1];
+        if (part2.length() > 3 && part2.charAt( 2 ) == '5' || part2.length() >= 3 && part2.charAt( 2 ) > '5') {
+            res = Math.round( res * 100.0 ) / 100.0;
+        } else {
+            res = Math.floor( res * 100.0 ) / 100.0;
+        }
+        return res;
+    }
+
+    private  void generateResults()
+    {
+        double res;
+        for (int i = 0; i < result.size(); i++) {
+            res = result.get( i );
+            resultOutput = resultOutput + Double.toString( ++res );
+            java.lang.String resString = Double.toString( res );
+            java.lang.String[] parts = resString.split( "\\." );
+            java.lang.String part1 = parts[0];
+            java.lang.String part2 = parts[1];
+            if (part2.length() == 1) {
+                resultOutput = resultOutput + Integer.toString( 0 );
+            }
+            resultOutput = resultOutput + " - " + resultInInterval.get( i );
+            if (i < result.size() - 1) {
+                resultOutput = resultOutput + "\n";
+            }
+        }
+    }
+
+    public  java.lang.String getResultOutput()
+    {
+        return resultOutput;
+    }
+
+    public static  void main( java.lang.String[] args )
+    {
+        BBSecurity bbs = new BBSecurity( args );
+        System.out.println( bbs.getResultOutput() );
+    }
+
+}
---
+++ src/mutans/SDL_76/BBSecurityTest.java
@@ -0,0 +1,197 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class BBSecurityTest {
+  /*
+  * * Start a) equivalence partitioning
+  */
+  @Test
+  public void test1a1(){
+    String[] args = {"2","0","100","60","100","20","100","50","100"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("49.44 - da\n50.0 - da", bbs.getResultOutput());
+  }
+  @Test
+  public void test1a2(){
+    String[] args = {};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1a3(){
+    String[] args = {"0.5"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1a4(){
+    String[] args = {"100001.7"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1a5(){
+    String[] args = {"17","1000001.9"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1a6(){
+    String[] args = {"3","13","1111333.8"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1a7(){
+    String[] args = {"1","0","893","2123456.1"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1a8(){
+    String[] args = {"2","30","1234","40","3321123.5"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1a9(){
+    String[] args = {"2","30","1234","40","43","4000987.9"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1a10(){
+    String[] args = {"2","30","1234","40","43","40"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  /*
+  * * Stop a) equivalence partitioning
+  */
+  /*
+  * * Start b) boundary value analysis
+  */
+  @Test
+  public void test1b1(){
+    String[] args = {"3","0","1000000","60","90","36","73","133","13","11","100","11"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("77.19 - da\n36.8 - da\n100.0 - da", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b2(){
+    String[] args = {"0"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b3(){
+    String[] args = {"100001"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b4(){
+    String[] args = {"1","-1"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b5(){
+    String[] args = {"1","1000001"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b6(){
+    String[] args = {"1","1","-1"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b7(){
+    String[] args = {"1","1","1000001"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b8(){
+    String[] args = {"1","1","10000","-1"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b9(){
+    String[] args = {"1","1","10000","1234567"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b10(){
+    String[] args = {"1","1","10000","12","-1"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b11(){
+    String[] args = {"1","1","10000","12","9879345"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b12(){
+    String[] args = {"1","1","10000","45","600","-19"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  @Test
+  public void test1b13(){
+    String[] args = {"1","1","10000","45","600","1234987"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("Invalid input", bbs.getResultOutput());
+  }
+  /*
+  * * Stop b) boundary value analysis
+  */
+  /*
+  * * Start b) cause-effect graphing
+  */
+  @Test
+  public void test1c1(){
+    String[] args = {"1","0","1000000","60","90","36"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("77.19 - da", bbs.getResultOutput());
+  }
+  @Test
+  public void test1c2(){
+    String[] args = {"1","0","1000000","43","86","33"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("95.23 - da", bbs.getResultOutput());
+  }
+  @Test
+  public void test1c3(){
+    String[] args = {"1","0","100000","123","756","123"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("756.0 - da", bbs.getResultOutput());
+  }
+  @Test
+  public void test1c4(){
+    String[] args = {"1","0","10","60","90","36"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("77.19 - nu", bbs.getResultOutput());
+  }
+  @Test
+  public void test1c5(){
+    String[] args = {"1","0","10","43","86","33"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("95.23 - nu", bbs.getResultOutput());
+  }
+  @Test
+  public void test1c6(){
+    String[] args = {"1","0","10","123","756","123"};
+    BBSecurity bbs = new BBSecurity(args);
+    assertEquals("756.0 - nu", bbs.getResultOutput());
+  }
+  /*
+  * * Stop b) cause-effect graphing
+  */
+}
